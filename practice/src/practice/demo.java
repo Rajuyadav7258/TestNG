3f1b30589b04f5baed75174428a341617aec019b
Raju yadav <150435502+Rajuyadav7258@users.noreply.github.com>
2023-11-24 12:18:00 +0530

    Update demo.java
    i have add 1 method

--- practice/src/practice/demo.java
+++ practice/src/practice/demo.java
@@ -3,7 +3,13 @@ package practice;
 public class demo {
 
 	public static void main(String[] args) {
-		System.out.println("Demo");
+		System.out.println("Raju ");
 	}
+
+
+public static void m2(){
+
+System.out.println("Yadav ");
+System.out.println(" sonu ")
 
 }
