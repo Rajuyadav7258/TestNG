d805e30e2286413da4b9684c9fc2885c88dd2d19
kushal-gentleman <pathakkushal733@gmail.com>
2023-11-24 12:06:24 +0530

    Update demo.java

--- practice/src/practice/demo.java
+++ practice/src/practice/demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("Demo");
          System.out.println("Kushal Pathak");
+System.out.println("Kushal@1234");
 	}
 
 }
